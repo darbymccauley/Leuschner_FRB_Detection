6a6b97ccdfe2c76904dee049fc515ec050de4860
darbymccauley <darbymccauley@berkeley.edu>
2022-11-14 16:32:04 -0800

    square wave test

---
+++ scripts/rpi4/testing.py
@@ -0,0 +1,28 @@
+# import numpy as np
+import RPi.GPIO as GPIO
+# import sys
+# from time import sleep
+
+
+# Set up pin to be used
+gpio_pin = 25
+
+# Count to N
+N = 10000
+
+# Initialize use of GPIOs
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(gpio_pins, GPIO.OUT) # set the pin to be an output
+
+while True:
+    GPIO.output(gpio_pin, GPIO.LOW)
+    print('Changed low')
+    cnt = 0
+    for n in range(N):
+        cnt += 1
+    GPIO.output(gpio_pin, GPIO.HIGH)
+    print('Changed high')
+    cnt = 0
+    for n in range(N):
+        cnt += 1
