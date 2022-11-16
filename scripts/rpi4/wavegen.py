0e590ddb624a594f5282d26c29f4330f1fc4dc29
darbymccauley <darbymccauley@berkeley.edu>
2022-11-16 13:25:56 -0800

    code cleanup

--- scripts/rpi4/wavegen.py
+++ scripts/rpi4/wavegen.py
@@ -1,65 +1,12 @@
 import RPi.GPIO as GPIO
 import numpy as np
 
-### IMPORT RFI NOTE ###
-# XXX When "blanked" -- tone for PTS3200 at 1.9604 GHz at -48dBm, with floor at -58 dBm
-#                                       DC to 160 MHz at -48 dBm
-
-### UB' QUESTIONABLE -- switched it out with another piece from another board (RESOLVED)
-### Rising edges (RESOLVED)
-### cable length from rpi to board needs adjusting
-### board supports 40-bits but we only gave 38, causing a weird 2 bit shift (RESOLVED)
-
 
 ### HOW TO CHANGE THE GPIO DRIVER STRENGTH: ###
 # Initialize comms: sudo pigpiod
 # Read/Get driver strength from Pad 0 (GPIOs 0-27): pigs padg 0
 # Set driver strength of Pad 0 to X mA: pigs pads 0 X
 
-
-### FORMAT OF PTS BIT SIGNIFICANCE: ###
-# 2^3  2^2  2^1  2^0
-#  8    4    2    1
-
-# pts_pins_1GHz = [44, 43]
-# pts_pins_100MHz = [41, 40, 16, 15]
-# pts_pins_10MHz = [20, 19, 18, 17]
-# pts_pins_1MHz = [27, 26, 2, 1]
-# pts_pins_100kHz = [29, 28, 4, 3]
-# pts_pins_10kHz = [31, 30, 6, 5]
-# pts_pins_1kHz = [33, 32, 8, 7]
-# pts_pins_100Hz = [35]
-# pts_pins_10Hz= []
-# pts_pins_1Hz = []
-
-# pts_pins = [pts_pins_1GHz, 
-#             pts_pins_100MHz, pts_pins_10MHz, pts_pins_1MHz, 
-#             pts_pins_100kHz, pts_pins_10kHz, pts_pins_1kHz, 
-#             pts_pins_100Hz, pts_pins_10Hz, pts_pins_1Hz]
-
-# pts_pins_LE = [23, 24, 25, 26]
-# pts_pins_remote = [42]
-
-
-# gpio_pins_1GHz = [26, 25]
-# gpio_pins_100MHz = [24, 23, 22, 21]
-# gpio_pins_10MHz = [20, 19, 18, 17]
-# gpio_pins_1MHz = [16, 15, 14, 13]
-# gpio_pins_100kHz = [12, 11, 10, 9]
-# gpio_pins_10kHz = [8, 7, 6, 5]
-# gpio_pins_1kHz = [4, 3, 2, 1]
-# gpio_pins_100Hz = [0]
-# gpio_pins_10Hz = []
-# gpio_pins_1Hz = []
-
-# gpio_pins = [gpio_pins_1GHz, 
-#              gpio_pins_100MHz, gpio_pins_10MHz, gpio_pins_1MHz, 
-#              gpio_pins_100kHz, gpio_pins_10kHz, gpio_pins_1kHz, 
-#              gpio_pins_100Hz, gpio_pins_10Hz, gpio_pins_1Hz]
-
-# gpio_pins_LE = [26]
-# gpio_pins_remote = [27]
-# # LATCH ENABLE AND REMOTE ARE NOT NEEDED ANYMORE -- HARDCODED 
 
 
 # GPIO pins
@@ -78,10 +25,10 @@ def convert_to_bins(frequency, model='PTS3200'):
         - model (str): PTS model used. Default is PTS3200.
           Accepts PTS3200, PTS500, PTS300.
     Returns:
-        - A list of len 10 (for 10 decimal places from Hz to GHz),
-          with each element in the list containing a binary nibble,
-          with the exception of the GHz element, which contains a
-          half-nibble (in accordance with PTS3200 frequency limits).
+        - A list of len 10 (for 10 decimal places from GHz to Hz),
+          with each element in the list containing a binary nibble.
+          This corresponds to a 40-bit total. The nibbles read from
+          most to least significant bit.
     """
     frequency = int(np.round(frequency)) # fractions not allowed -- round and make an integer value
     if model == 'PTS3200':
@@ -104,7 +51,7 @@ def convert_to_bins(frequency, model='PTS3200'):
     return binary_numbers
 
 
-def shift_in_new_freq(binary_numbers):
+def load_frequency(binary_numbers):
     """
     Reads a set of binary converted frequency values and 
     loads the data into the appropriate GPIO pin.
@@ -154,12 +101,12 @@ def continuous_wave(freq):
         - freq (int)|[Hz]: Desired frequency
     """
     binary_list = convert_to_bins(freq) # convert freq from decimal to binary
-    shift_in_new_freq(binary_list) # load data to GPIO
+    load_frequency(binary_list) # load data to GPIO
     usleep(2) # conservative wait after data as been serially shifted before doing parallel load
     send_command() # send data to PTS
 
 
-def dummy_routine():
+def test_frequency_switches():
     """
     Temporary test function.
     """
@@ -199,10 +146,10 @@ def usleep(time):
     """
     Sleep for a given number of microseconds.
     WARNING: Needs to be calibrated according to used hardware.
-        (Current rough estimate for RPi4: 2400 cnts = 1 ms)
+    (Current rough estimate for RPi4 + PTS3200: 2400 cnts = 1 ms)
 
     Inputs:
-        - time [microseconds]: time of delay
+        - time [us]: time of delay
     """
     # Convert time to count number, N
     const = 2400/1e3
@@ -213,6 +160,9 @@ def usleep(time):
 
 
 def blank():
+    """
+    Clear signal, reset to 0 Hz, and reset clocks.
+    """
     N = 50
     GPIO.output(gpio_sclk_pin, GPIO.HIGH) # set off
     for j in range(2):
