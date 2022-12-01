3d16c13457f757c0a4659aa7bd5c2b66f7cc8ebe
darbymccauley <darbymccauley@berkeley.edu>
2022-12-01 11:40:14 -0800

    reorganize

--- sims/simfrb.py
+++ sims/simfrb.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
+from tqdm import tqdm
 
 
 def DM_delay(DM, freq):
@@ -18,20 +19,20 @@ def DM_delay(DM, freq):
     A = DM*const*1e3 # 1e3 for conversion to ms
     return A/(freq**2)
 
-def make_frb(nspec=1024, nchans=2048, DM=332.72, pulse_width=2.12, f_min=1150, f_max=1650, t_samp=0.064, offset=0, plot=False):
+def make_frb(nspec=4096, nchans=4096, DM=332.72, pulse_width=2.12, f_min=1150, f_max=1650, t_samp=0.06128, SNR=2, plot=False):
     """
     Simulates a fast radio burst (FRB) observation given a 
     certain set of parameters.
-
     Inputs:
         - nspec: number of spectra
         - nchans: number of frequency channels
         - DM [pc*cm^-3]: dispersion measure
         - pulse_width [ms]: width of FRB pulse
-        - f_min [MHz]: lower frequency of bandwidth
-        - f_max [MHz]: upper frequency of bandwidth
+        - f_min [MHz]: lower frequency of band
+        - f_max [MHz]: upper frequency of band
         - t_samp [ms]: sampling time/time resolution
         - offset [ms]: offset the pulse start time
+        - SNR: signal-to-noise level
         - plot (bool): plot output results
     Returns:
         - Power matrix of shape (nchans, nspec)
@@ -41,52 +42,151 @@ def make_frb(nspec=1024, nchans=2048, DM=332.72, pulse_width=2.12, f_min=1150, f
     t_min = 0
     t_max = t_samp*nspec
 
-    signal_power = 0.5
-    
-    # td0 = DM_delay(DM, f_max)
-    # tds = DM_delay(DM, freqs) - td0 + offset
-    # bins = tds/t_samp
-    delays = DM_delay(DM, freqs) + offset
+    delays = DM_delay(DM, freqs)
     delay_bins = delays/t_samp
     rounded_bins = np.round(delay_bins).astype(int)
 
     pulse_bin_width = np.round(pulse_width/t_samp).astype(int)
 
-    Image = np.random.normal(size=shape, loc=1, scale=0.2) # XXX
+    noise_std = 0.2 # XXX
+    Image = np.random.normal(size=shape, loc=1, scale=noise_std)
+    noise_power = 1
+    signal_power = SNR*noise_power
 
     FWHM_frac = 2*np.sqrt(2*np.log(2))
-    sigma = pulse_width/FWHM_frac
-    window_len = 11
-    gauss_window = signal.windows.gaussian(window_len, sigma/t_samp)
+    sigma = pulse_bin_width/FWHM_frac
+    window_len = pulse_bin_width
+    gauss_window = signal.windows.gaussian(window_len, sigma)
 
+    Image[:, :pulse_bin_width] += signal_power
     for i in range(len(Image)):
         if rounded_bins[i] > nspec:
-            continue # so the tail of the pulse doesn't loop back around
-        Image[i] = np.roll(Image[i], rounded_bins[i]) # shift by DM delay to produce dispersion curve
-        Image[i, rounded_bins[i]:rounded_bins[i]+pulse_bin_width] += signal_power # give power to curve
-        
-    # for spec in range(nspec):
+            continue # so the tail of the pusle doesn't loop back around
+        Image[i] = np.roll(Image[i], rounded_bins[i])
+        # Image[i, :] *= gauss_window 
 
-
-
-    
     if plot:
-        fig, ax = plt.subplots(constrained_layout=True)
-        im = ax.imshow(Image, aspect='auto', origin='lower', extent=[t_min, t_max, f_min, f_max])
-
-        ax.set_xlabel('Time [ms]')
-        ax.set_ylabel('Frequency [MHz]')
-        ax.set_xlim(t_min, t_max)
-        ax.set_ylim(f_min, f_max)
-
-        ax2 = ax.twinx()
-        ax2.set_ylim(0, nchans)
-        ax2.set_ylabel('Channel', rotation=270, labelpad=10)
+        plot_frb(Image, t_min, t_max, f_min, f_max)
         
-        ax3 = ax.twiny()
-        ax3.set_xlim(0, nspec)
-        ax3.set_xlabel('Spectrum', labelpad=10)
-
-        plt.show();
-
     return Image
+
+
+def plot_frb(Image, t_min, t_max, f_min, f_max):
+    """
+    Plot FRB data.
+    
+    Inputs:
+        - Image: Power matrix of shape (nchans, nspec)
+        - t_min [ms]: start time
+        - t_max [ms]: end time
+        - f_min [MHz]: lower frequency of band
+        - f_max [MHz]: upper frequency of band
+    Returns: plot of Image with time and frequency main axes,
+      and channel and spectrum number minor axes.
+    """
+    nchans, nspec = Image.shape
+    fig, ax = plt.subplots(constrained_layout=True)
+    im = ax.imshow(Image, aspect='auto', origin='lower', extent=[t_min, t_max, f_min, f_max])
+
+    ax.set_xlabel('Time [ms]')
+    ax.set_ylabel('Frequency [MHz]')
+    ax.set_xlim(t_min, t_max)
+    ax.set_ylim(f_min, f_max)
+
+    ax2 = ax.twinx()
+    ax2.set_ylim(0, nchans)
+    ax2.set_ylabel('Channel', rotation=270, labelpad=10)
+    
+    ax3 = ax.twiny()
+    ax3.set_xlim(0, nspec)
+    ax3.set_xlabel('Spectrum', labelpad=10)
+
+    plt.show();
+
+
+def collect_data(obs_time, file_path, nchans=4096, DM=332.72, pulse_width=2.12, f_min=1150, f_max=1650, t_samp=0.0612, SNR=2):
+    """
+    Collect simulated FRB data and chunk the spectra into
+    saved datafiles.
+
+    Inputs:
+        - obs_time [s]: observation time in seconds
+        - file_path (str): path to save collected files to
+        - nchans (int): number of frequency channels
+        - DM [pc*cm^-3]: dispersion measure
+        - pulse_width [ms]: time width of FRB pulse
+        - f_min [MHz]: lower frequency of band
+        - f_max [MHz]: upper frequency of band
+        - t_samp [ms]: sampling time/time resolution
+        - SNR: signal-to-noise ratio
+    Returns: A series of data files saved to specified file 
+      path, each containing 1s worth of data.
+    """
+    obs_time_ms = obs_time*1e3 # convert s to ms
+    nspec_1s = np.round(1e3/t_samp).astype(int) # number of spectra in 1 second 
+    nspec_tot = np.round(obs_time_ms/t_samp).astype(int) # total number of spectra to be collected in observing time
+    nblocks = np.round(nspec_tot/nspec_1s).astype(int)
+
+    Image = make_frb(nspec=nspec_tot, nchans=nchans, DM=DM, pulse_width=pulse_width, f_min=f_min, f_max=f_max, t_samp=t_samp, SNR=SNR)
+
+    spec0 = 0
+    for block in range(1, nblocks+1):
+        if block == np.max(nblocks)+1: # needed in case last number of spectra in last block is not equal to nspec_1s
+            data_chunk = Image[:, spec0:]
+            np.savetxt(file_path+'/block_'+str(block)+'.txt', data_chunk)
+        data_chunk = Image[:, spec0:block*nspec_1s]
+        np.savetxt(file_path+'/block_'+str(block)+'.txt', data_chunk)
+        spec0 = block*nspec_1s
+
+
+def half_block(Image):
+    """
+    Split data from data files into halves for
+    use in later power analysis.
+    
+    Inputs:
+        - Image: data from data file
+    Returns: Two matrixes each 0.5s in time width.
+    """
+    # data = np.loadtxt(file)
+    half_index = np.round(Image.shape[1]/2).astype(int)
+    first_half, second_half = Image[:, :half_index], Image[:, half_index:]
+    return first_half, second_half
+
+
+def find_pulse(data_file, minimum_sigma=2):
+    """
+    Look for pulses in the data files and save 
+    interesting data to a singular file.
+
+    Inputs: 
+        - data_file: data file containing power matrix
+        - minimum_sigma: Minimum average power signal 
+          that warrents a saving of data set
+    Returns: XXX
+    """
+    data = np.loadtxt(data_file)
+    first_half_data, second_half_data = half_block(data)
+    blocks = [['first half of '+data_file, first_half_data], ['second half of '+data_file, second_half_data]]
+    for name, half in blocks:
+        f = open(FILENAME, 'a') # append mode
+        if half.mean() > minimum_sigma:
+            print(f'FRB pulse found in {name}. Saving...')
+
+
+
+# def minimize_nspec(date_file, minimum_sigma):
+#     Image = np.loadtxt(data_file)
+#     nchans, nspec = Image.shape
+#     log2_nspec = int(np.log2(nspec))
+#     indices = []
+#     for i in range(log2_nspec):
+#         data = Image[:, 2**i:2**(i+1)]
+#         if data.mean() > minimum_sigma:
+#             indices.append(i)
+#     minimum = 2**(np.min(indices)) - 1  
+#     maximum = 2**(np.max(indices)+1)
+#     length = maximum - minimum
+#     end_index = int(2**np.ceil(np.log2(length)))
+#     return Image[:, minimum:minimum+end_index]
+    
---
+++ src/rpi4/wavegen.py
@@ -0,0 +1,311 @@
+import RPi.GPIO as GPIO
+import numpy as np
+import os
+
+
+### HOW TO CHANGE THE GPIO DRIVER STRENGTH: ###
+# Initialize comms: sudo pigpiod
+# Read/Get driver strength from Pad 0 (GPIOs 0-27): pigs padg 0
+# Set driver strength of Pad 0 to X mA: pigs pads 0 X
+
+# Set GPIO driver strength 
+os.system('sudo pigpiod') 
+os.system('pigs pads 0 4') # set driver strength of Pad 0 (GPIOs 0-27) to 4 mA
+
+
+# GPIO pins
+GPIO_DATA_PIN = 23 # data pin
+GPIO_SCLK_PIN = 22 # serial clock pin
+GPIO_PCLK_PIN = 24 # parallel clock pin
+# GPIO_PINS = [GPIO_DATA_PIN, GPIO_SCLK_PIN, GPIO_PCLK_PIN]
+
+# PTS model
+MODEL = 'PTS3200'
+
+# Rather than zeroing the signal (0 Hz) when a sweep is not occuring,
+# we choose a random but easily recognizable frequency for the purpose
+# of debugging and ensuring the system is performing as expected. This
+# frequency will later be filtered out via a 250 MHz high-pass filter.
+NO_SIGNAL = 7.2e6 # Hz
+
+class WaveGen():
+
+    def __init__(self, gpio_data_pin=GPIO_DATA_PIN, gpio_sclk_pin=GPIO_SCLK_PIN, gpio_pclk_pin=GPIO_PCLK_PIN, model=MODEL, no_signal=NO_SIGNAL):
+        """
+        Instantiate use of PTS and RPi GPIO pins.
+        """
+        self.model = model
+        self.gpio_data_pin = gpio_data_pin
+        self.gpio_sclk_pin = gpio_sclk_pin
+        self.gpio_pclk_pin = gpio_pclk_pin
+        self.gpio_pins = [self.gpio_data_pin, self.gpio_sclk_pin, self.gpio_pclk_pin]
+        self.no_signal = no_signal
+
+        GPIO.setwarnings(False) # ignore RPi.GPIO internal messaging
+        GPIO.setmode(GPIO.BCM) # use GPIO numbers rather than pin numbers
+        # define GPIO pins as outputs
+        GPIO.setup(self.gpio_pins, GPIO.OUT)
+        # set initial level of GPIO pins
+        GPIO.output(self.gpio_data_pin, GPIO.LOW)
+        GPIO.output(self.gpio_sclk_pin, GPIO.HIGH)
+        GPIO.output(self.gpio_pclk_pin, GPIO.HIGH)
+
+
+    def _convert_to_bins(self, frequency, model='PTS3200'):
+        """
+        Convert a given input into its binary counterpart.
+
+        Inputs:
+            - frequency (int)|[Hz]: Input frequency
+            - model (str): PTS model used. Default is PTS3200.
+              Accepts PTS3200, PTS500, PTS300.
+        Returns:
+            - A list of len 10 (for 10 decimal places from GHz to Hz),
+              with each element in the list containing a binary nibble.
+              This corresponds to a 40-bit total. The nibbles read from
+              most to least significant bit.
+        """
+        min_freq = 1e6
+        if self.model == 'PTS3200':
+            max_freq = 3199999999
+        elif self.model == 'PTS500': 
+            max_freq = 500e6
+        else: # assume PTS300
+            max_freq = 300e6
+        rounded_frequency = int(np.round(frequency)) # fractions not allowed
+        if rounded_frequency > max_freq or rounded_frequency < min_freq: # upper and lower frequency bounds 
+            print('WARNING: Input frequency {0} is out of range for model {1} ({2} - {3} Hz).'.format(frequency, model, max_freq, min_freq))
+            if rounded_frequency > max_freq:
+                rounded_frequency = max_freq # set to upper limit
+            elif rounded_frequency < min_freq:
+                rounded_frequency = min_freq # set to lower limit
+        Frequency = str(rounded_frequency).zfill(10)
+        freq = [int(n) for n in Frequency]
+        binary_numbers = []
+        for i, n in enumerate(freq):
+            bin_num = np.binary_repr(n, width=4)
+            binary_numbers.append(bin_num)
+        return binary_numbers
+
+
+    def _load_frequency(self, binary_numbers):
+        """
+        Reads a set of binary converted frequency values and 
+        loads the data into the appropriate GPIO pin.
+
+        Inputs:
+            - binary_numbers: list of 10 nibbles containing
+              frequency information
+        """
+        split_binary_numbers = []
+        for num in binary_numbers:
+            split = [int(n) for n in num]
+            split_binary_numbers.append(split)
+        GPIO.output(self.gpio_sclk_pin, GPIO.HIGH) # set serial clk to off state
+        GPIO.output(self.gpio_pclk_pin, GPIO.HIGH) # set parallel clk to off state
+        bit_cnt = 0
+        for i in range(9, -1, -1): # count from most to least significant bit
+            for j in range(len(split_binary_numbers[i])-1, -1, -1):
+                if split_binary_numbers[i][j] == 0:
+                    # print('Shifting in a 0 at', bit_cnt)
+                    GPIO.output(self.gpio_data_pin, GPIO.LOW)
+                elif split_binary_numbers[i][j] == 1:
+                    # print('Shifting in a 1 at', bit_cnt)
+                    GPIO.output(self.gpio_data_pin, GPIO.HIGH)
+                # XXX User interaction for bit by bit input for debugging and probing
+                self._usleep(2) # let the data settle before pulsing clk
+
+                GPIO.output(self.gpio_sclk_pin, GPIO.LOW)
+                self._usleep(5) # stretch out clk pulse to be conservative
+                GPIO.output(self.gpio_sclk_pin, GPIO.HIGH) 
+                bit_cnt += 1
+        
+
+    def _send_command(self):
+        """
+        Triggers send of frequency from RPi to PTS.
+        """
+        # User interaction so we have control over change -- Verbose commands XXX
+        GPIO.output(self.gpio_pclk_pin, GPIO.LOW) # triggers send to PTS
+        GPIO.output(self.gpio_pclk_pin, GPIO.HIGH) # return parallel clock to off state
+
+
+    def continuous_wave(self, freq):
+        """
+        Generates a continuous wave of a specified frequency.
+        
+        Inputs:
+            - freq (int)|[Hz]: Desired frequency in decimal representation
+        """
+        self._make_wave(freq, decimal=True)
+
+
+    def _convert_freq_list(self, freqs, model='PTS3200'):
+        """
+        Convert an entire list of frequencies into binary
+        in preparation for sweep functions.
+
+        Inputs:
+            - freqs [Hz]: list of frequencies
+            - model (str): PTS model used. Default is PTS3200.
+              Accepts PTS3200, PTS500, PTS300.
+        """
+        bin_freqs = self._convert_to_bins(freqs, model)
+        return bin_freqs
+
+
+    def _make_wave(self, freq, decimal=False):
+        """
+        Load and generate a wave at the specified frequency.
+
+        Inputs:
+            - freq [Hz]: Desired frequency
+            - decimal (bool): is input freq given in decimal 
+              or binary form?
+        """
+        if decimal: # if frequency is given in decimal form:
+            binary_list = self._convert_to_bins(freq) # convert freq from decimal to binary
+        binary_list = freq # already in binary form
+        self._load_frequency(binary_list) # load binary data to GPIO
+        self._usleep(2) # conservative wait after data has been serially shifted before doing parallel load
+        self._send_command() # send data to PTS
+
+
+    def cleanup_gpio(self):
+        """
+        Cleanup GPIOs to ensure no damage is done to RPi.
+        """
+        GPIO.cleanup()
+
+
+    def _usleep(self, time):
+        """
+        Sleep for a given number of microseconds.
+        WARNING: Needs to be calibrated according to used hardware.
+        (Current estimate for RPi4 + PTS3200: 2400 cnts = 1 ms)
+
+        Inputs:
+            - time [us]: time of delay
+        """
+        # Convert time to count number, N
+        const = 2400/1e3
+        N = int(np.round(const*time))
+        cnt = 0
+        for i in range(N):
+            cnt += 1
+
+
+    def blank(self):
+        """
+        Clear signal and reset clocks.
+        """
+        N = 50
+        GPIO.output(self.gpio_sclk_pin, GPIO.HIGH) # set off
+        for j in range(2):
+            for i in range(N):
+                GPIO.output(self.gpio_sclk_pin, GPIO.LOW)
+                GPIO.output(self.gpio_sclk_pin, GPIO.HIGH)
+            self._send_command()
+       
+
+
+    def linear_sweep(self, f_min=1150e6, f_max=1650e6, nchans= 2048, dt=1000, model='PTS3200', continuous=False):
+        """
+        Generate a continuous linear (simple) sweep.
+
+        Inputs:
+            - f_min (float)|[Hz]: minimum frequency of sweep
+            - f_max (float)|[Hz]: maximum frequency of sweep
+            - nchans (int): number of frequency channels
+            - dt (float)|[us]: time until next frequncy change
+            - continuous (bool): single or repeating sweep?
+        """
+        freqs = np.linspace(f_min, f_max, nchans)
+        bin_freqs = self._convert_freq_list(freqs, model)
+        while continuous:
+            for f in bin_freqs:
+                self._make_wave(f)
+                self._usleep(dt)
+        for f in bin_freqs:
+            self._make_wave(f)
+            self._usleep(dt)
+
+
+    def _dm_delay(self, DM, freq):
+        """
+        Computes the frequency-dependent dispersion measure
+        time delay.
+        
+        Inputs:
+            - DM (float)|[pc*cm^-3]: dispersion measure
+            - freq (float)|[Hz]: frequency
+        Returns: pulse time delay in [us]
+        """
+        const = 4140
+        A = DM*const*1e6 # 1e6 for conversion to us
+        return A/((freq/1e6)**2)
+
+
+    def dm_sweep(self, DM=332.72, f_min=1150e6, f_max=1650e6, dt=1000, model='PTS3200', continuous=False):
+        """
+        Generates a frequency sweep that mirrors that caused
+        by dispersion measure influence.
+
+        Inputs:
+            - DM (float)|[pc*cm^-3]: dispersion measure (default 
+              is DM for SGR1935+2154)
+            - f_min (float)|[Hz]: minimum frequency of sweep
+            - f_max (float)|[Hz]: maximum frequency of sweep
+            - dt (float)|[us]: sweep update interval
+            - model (str): PTS model used. Default is PTS3200.
+              Accepts PTS3200, PTS500, PTS300.
+            - continuous (bool): single or repeating sweep?
+        """
+        const = 4140e6
+        A = const*DM
+        t0 = self._dm_delay(DM, f_max)
+        tf = self._dm_delay(DM, f_min)
+        ts = np.arange(t0, tf+dt, dt)
+        freqs = np.sqrt(A/ts)*1e6 # 1e6 for proper conversion of freqs into Hz -- these frequencies will be sent to the PTS
+        bin_freqs = self._convert_freq_list(freqs, model) # convert frequencies into binary form
+        while continuous:
+            for f in bin_freqs:
+                self._make_wave(f)
+                self._usleep(dt)
+        for f in bin_freqs:
+            self._make_wave(f)
+            self._usleep(dt)
+
+
+    def mock_obs(self, wait_time, DM=332.72, f_min=1150e6, f_max=1650e6, dt=1000, model='PTS3200'):
+        """
+        Simulate an FRB observation in collected time-dependent voltage data.
+
+        Inputs:
+            - wait_time (float)|[s]: time until FRB pulse/sweep begins
+            - DM (float)|[pc*cm^-3]: dispersion measure (default 
+              is DM for SGR1935+2154)
+            - f_min (float)|[Hz]: minimum frequency of sweep
+            - f_max (float)|[Hz]: maximum frequency of sweep
+            - dt (float)|[us]: sweep update interval
+            - model (str): PTS model used. Default is PTS3200.
+              Accepts PTS3200, PTS500, PTS300.
+            - save (bool): save mock observation to file?
+
+        Returns:
+            - If save, file containing voltage-time data.
+        """
+        wait_time_us = wait_time*1e6 # convert to microseconds
+        self.continuous_wave(self.no_signal) # "no signal" signal
+        self._usleep(wait_time_us)
+        self.dm_sweep(DM, f_min, f_max, dt, model, continuous=False) # FRB sweep
+        self.continuous_wave(self.no_signal) # go back to "no signal" signal
+        # XXX Add saving ability
+
+
+
+        
+
+
+
+
