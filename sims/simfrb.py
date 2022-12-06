2c67e4abbe29219c633bebd6aab187560f1b3430
darbymccauley <darbymccauley@berkeley.edu>
2022-12-06 12:42:38 -0800

    reorganize

--- sims/simfrb.py
+++ sims/simfrb.py
@@ -1,3 +1,8 @@
+#################################
+# Computer generated FRB signal #
+#################################
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
